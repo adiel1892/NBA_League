7886969a45d9aba849b190caf8d251ab80523033
Adiel Ben Meir <adiel1892@gmail.com>
2022-07-12 21:45:52 +0300

    add

--- League.py
+++ League.py
@@ -1,3 +1,5 @@
+import functools
+
 from Team import Team
 
 
@@ -24,11 +26,11 @@ class League:
     def longest_wins_strike(self):
         res = 0
         index = 0
-        for i in range(0,len(self.teams)):
+        for i in range(0, len(self.teams)):
             if self.teams[i].longest_wins_strike > res:
                 res = self.teams[i].longest_wins_strike
                 index = i
-        print("The longest wins strike is " , res , " by " , self.teams[index].name)
+        print("The longest wins strike is ", res, " by ", self.teams[index].name)
         return res
 
     def longest_losses_strike(self):
@@ -46,10 +48,70 @@ class League:
         for i in range(0, len(self.teams)):
             if self.teams[i].positive_points - self.teams[i].negative_points > 0:
                 res += 1
-        print("The longest losses strike is ", res, " by ", self.teams[index].name)
+        print("The number of teams with positive points is " , res)
         return res
 
+    def most_positive_points_team(self):
+        points = self.teams[0].positive_points
+        res = 0
+        for i in range(0, len(self.teams)):
+            if self.teams[i].positive_points > points:
+                points = self.teams[i].positive_points
+                res = i
+        print("The team with the most positive points in the league is ", self.teams[res].name, " with ", points,
+              " points!")
+        return self.teams[res].name
+
+    def lowest_positive_points_team(self):
+        points = self.teams[0].positive_points
+        res = 0
+        for i in range(0, len(self.teams)):
+            if self.teams[i].positive_points < points:
+                points = self.teams[i].positive_points
+                res = i
+        print("The team with the lowest positive points in the league is ", self.teams[res].name, " with ", points,
+              " points!")
+        return self.teams[res].name
+
+    def most_negative_points_team(self):
+        points = self.teams[0].negative_points
+        res = 0
+        for i in range(0, len(self.teams)):
+            if self.teams[i].negative_points > points:
+                points = self.teams[i].negative_points
+                res = i
+        print("The team with the most negative points in the league is ", self.teams[res].name, " with ", points,
+              " points!")
+        return self.teams[res].name
 
 
+    def lowest_negative_points_team(self):
+        points = self.teams[0].negative_points
+        res = 0
+        for i in range(0, len(self.teams)):
+            if self.teams[i].negative_points < points:
+                points = self.teams[i].negative_points
+                res = i
+        print("The team with the lowest negative points in the league is ", self.teams[res].name, " with ", points,
+              " points!")
+        return self.teams[res].name
+
+    def compare_teams(self , a: Team , b: Team):
+        if a.wins > b.wins:
+            return True
+        elif a.wins < b.wins:
+            return False
+        elif a.positive_points - a.negative_points > b.positive_points - b.negative_points:
+            return True
+        else:
+            return False
+
+    def show_table_when_season_over(self):
+       sorted(self.teams , key=functools.cmp_to_key(self.compare_teams()))
+
+    def print_top_teams(self , top):
+        print("Top " , top , " teams:")
+        for i in range(0,top):
+            print(i + 1 , ") " , self.teams[i].name)
 
 
